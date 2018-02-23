a520535c102502c2eed77c76c309864a34732176
Harrison Bond <baronvonbadguy@gmail.com>
2018-02-22 21:35:15 -0500

    Improve some command responses

--- bluebot.js
+++ bluebot.js
@@ -11,12 +11,16 @@ bot.onText(/(\/[a-zA-Z]+)/, (msg, match) => {
   var command = match[0];
   commands = {
     '/website': 'https://www.etherblue.org/',
-    '/twitter': 'https://twitter.com/EthereumBlue',
+    '/twitter': 'You can keep in touch with us at our ' +
+                '[twitter](https://twitter.com/EthereumBlue}',
     '/telegram': 'https://t.me/joinchat/HBDo00IO49STMJuIwJi08g',
-    '/whitepaper': 'https://www.etherblue.org/whitepaper',
-    '/github': 'https://github.com/BlueCrypto/',
-    '/announcement': 'https://bitcointalk.org/index.php?topic=2279214.0',
-    '/discord': 'https://discord.gg/vKxnqxX',
+    '/whitepaper': 'You can read up on what we\'re trying to accomplish in' +
+                    ' our [whitepaper](https://www.etherblue.org/whitepaper)',
+    '/github': 'You can see what we\'re working on at our [github]' +
+                '(https://github.com/BlueCrypto/)',
+    '/announcement': 'You can see where it all started at the bitcointalk' +
+                      '[announcement](https://bitcointalk.org/index.php?topic=2279214.0)',
+    '/discord': 'You can join us for a chat in our [discord](https://discord.gg/vKxnqxX)',
     '/reddit': 'https://www.reddit.com/r/BlueCrypto/',
     '/cmc': 'https://coinmarketcap.com/currencies/ethereum-blue/',
     '/youtube': 'https://www.youtube.com/channel/UCNtv0tIgBYofh4LTWKKZj7A',
@@ -25,9 +29,10 @@ bot.onText(/(\/[a-zA-Z]+)/, (msg, match) => {
   var options = {
     'parse_mode': 'markdown',
     'disable_web_page_preview': true
-  }
+  };
+  var username = ''
   if (command in commands){
     bot.sendMessage(chatId, commands[command], options);
-  }
+  };
   bot.deleteMessage(chatId, msg.message_id);
 });
